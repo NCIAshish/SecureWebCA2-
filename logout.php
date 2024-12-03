85eb9b68e2b58d91580d91542835f212c75e698b
Ashish Nagargoje <x23205288@student.ncirl.ie>
2024-12-03 14:06:05 +0000

    2 Factor Authentication
    implemented PHPMailer to support 2fa

---
+++ logout.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+// Clear all session data
+session_destroy();
+
+// Clear all cookies
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time() - 3600, '/');
+        setcookie($name, '', time() - 3600, '/', $_SERVER['HTTP_HOST']);
+    }
+}
+
+// Redirect to index page
+header("Location: index.php");
+exit();
+?>
